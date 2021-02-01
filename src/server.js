702937c7ecd2fb8ce004d30a9b4b83d65b396143
Xinli WU <ryan7gm@gmail.com>
2021-02-01 11:45:11 +1100

    pkg update

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
-// import "core-js/stable";
-// import "regenerator-runtime/runtime";
-require('dotenv').config()
+import dotenv from 'dotenv';
 import express, { json } from 'express';
 import helmet from 'helmet';
 import router from './router.js';
 
+dotenv.config();
 const PORT = process.env.API_PORT || 3000;
 const app = express();
 
@@ -14,7 +13,6 @@ app.use((_req, res, next) => {
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
-  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
   next();
 });
 app.use("/", router);
@@ -23,4 +21,3 @@ app.listen(PORT, () => {
   console.log(`NODE_ENV=${process.env.NODE_ENV}`)
   console.log(`Server listening on port ${PORT}!`);
 });
-// module.exports = app;
