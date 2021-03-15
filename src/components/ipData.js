1cee43f6b31623177423128ac4b920c3597e6fe1
Xinli WU <ryan7gm@gmail.com>
2021-03-16 10:23:11 +1100

    geoData cleanup

--- src/components/ipData.js
+++ src/components/ipData.js
@@ -1,17 +1,17 @@
-import dns from 'dns';
-import axios from 'axios';
-import isIp from 'is-ip';
+import dns from "dns";
+import axios from "axios";
+import isIp from "is-ip";
 
 const dnsPromises = dns.promises;
 const options = {
   family: 4,
-  hints: dns.ADDRCONFIG | dns.V4MAPPED
+  hints: dns.ADDRCONFIG | dns.V4MAPPED,
 };
 
-const getGEOIPInfo = async params => {
+const getGEOIPInfo = async (params) => {
   const query = {
-    ip: isIp(params.query) ? params.query : null
-    , domain: isIp(params.query) ? null : params.query
+    ip: isIp(params.query) ? params.query : null,
+    domain: isIp(params.query) ? null : params.query,
   };
   let result = null;
   if (query.domain) {
@@ -20,28 +20,41 @@ const getGEOIPInfo = async params => {
       if (!isIp(res.address)) {
         throw {
           error: {
-            msg: 'DNS lookup error'
-          }
-        }
+            msg: "DNS lookup error",
+          },
+        };
       }
       query.ip = isIp(res.address) ? res.address : null;
     } catch (ex) {
       result = ex;
     }
-  };
+  }
 
   const url = `https://api.ipgeolocationapi.com/geolocate/${query.ip}`;
   try {
     const res = await axios.get(url);
     result = res.data;
-    result = { ...query, ...result }
+    result = { ...query, ...result };
+
+    const keepKey = [
+      "ip",
+      "domain",
+      "name",
+      "region",
+      "subregion",
+      "un_locode",
+      "languages_official",
+      "currency_code",
+    ];
+
+    Object.keys(result).forEach((itm) => {
+      if (!keepKey.includes(itm)) delete result[itm];
+    });
   } catch (ex) {
-    result = ex
+    result = ex;
   } finally {
     return result;
   }
-}
+};
 
-export {
-  getGEOIPInfo
-}
+export { getGEOIPInfo };
